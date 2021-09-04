e430dca9d8da7035c9a61081be94997ddfb5a4c6
nisacanyilmaz <nisa.canyilmaz@gmail.com>
2021-09-05 01:23:02 +0300

    updated

--- bolum2/operators.dart
+++ bolum2/operators.dart
@@ -2,7 +2,26 @@ main(List<String> args) {
   double sayi1 = 9;
   double sayi2 = 6;
 
-  print("$sayi1 + $sayi2 toplamı: ${sayi + sayi2}");
+  //Aritmetik operatorler
+  print("$sayi1 + $sayi2 toplamı: ${sayi1 + sayi2}");
 
   // % mod
+
+  //Atama ve karsilastirma ( <, >, ==, != vs...)
+  double sayi3 = 5;
+  sayi3 = sayi3 + 5;
+  print(sayi3);
+
+  sayi3 += 5;
+  print(sayi3);
+
+  //mantıksal op.
+
+  //&&, ||, !
+
+  bool k1 = true;
+  bool k2 = false;
+  print(k1 && k2);
+  print(k1 || k2);
+  print(!k1);
 }
