ff5920b32caa35e33f7cebe29adccc91ff207820
nisacanyilmaz <nisa.canyilmaz@gmail.com>
2021-09-04 16:27:40 +0300

    updated

--- bolum2/if_else.dart
+++ bolum2/if_else.dart
@@ -7,4 +7,16 @@ main(List<String> args) {
   } else {
     print("$sayi2 sayısı $sayi1 sayısından küçüktür");
   }
+
+  int notDegeri = 75;
+
+  if (notDegeri < 30) {
+    print("Harf notu FF");
+  } else if (notDegeri > 30 && notDegeri < 50) {
+    print("Harf notu CC");
+  } else if (notDegeri > 50 && notDegeri < 70) {
+    print("Harf notu BB");
+  } else if (notDegeri > 70 && notDegeri < 100) {
+    print("Harf notu AA");
+  }
 }
