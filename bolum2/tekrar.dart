ab0c6d486e237e14b0d2e41552c2a2f6c56703dc
nisacanyilmaz <nisa.canyilmaz@gmail.com>
2021-12-07 22:15:33 +0300

    updated

---
+++ bolum2/tekrar.dart
@@ -0,0 +1,27 @@
+void main(List<String> args) {
+  //3 tane double değişkeni oluşturup bunların ortalaması
+  /*
+  double a = 2.97;
+  double b = 4.45;
+  double c = 7.56;
+
+  double ortalama = (a + b + c) / 3;
+  print(ortalama);
+  */
+
+  //Kenarları girilen üçgenin çeşidini yazan program.
+  /*
+  int kenar1 = 10;
+  int kenar2 = 20;
+  int kenar3 = 30;
+
+  
+  if ((kenar1 == kenar2) && (kenar1 == kenar3)) {
+    print("eşkenar üçgen");
+  } else if (kenar1 == kenar2 || kenar1 == kenar3 || kenar2 == kenar2) {
+    print("ikizkenar üçgen");
+  } else {
+    print("çeşitkenar üçgen");
+  }
+  */
+}
